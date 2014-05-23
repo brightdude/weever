a706ab5bef17739cb05d2125efa7c21bd69785ff
marksuchov <marksuchov@gmail.com>
2014-05-22 18:26:26 -0700

    meetup channel creates to db
    The version abandons looping for each host memeber to get their profile
    data as that gets account throttled;

--- Server/routes/meetup.js
+++ Server/routes/meetup.js
@@ -1,93 +1,141 @@
-var meetup = require('meetup-api')('5f162b4a1e6c18144031593436255d38');
+var meetup = require('meetup-api')('291b5a6c1e4f4c49345a587c8203312');
+//var meetup= require('meetup-api')('5f162b4a1e6c18144031593436255d38');
+
 var mongoose = require('mongoose');
 var db = mongoose.connection;
-
-function Lib() {
-}
-Lib.prototype.GetEventList = function() {
-}
 db.on('error', console.error);
-// db.once('open', function() {
-// });
-
 mongoose.connect('localhost', 'data');
-
-var event_hosts = new mongoose.Schema({
-member_id: String, 
-member_name: String
-});
-
-var venue = new mongoose.Schema({
-address_1: String,
-address_2: String,
-address_3: String,
-city_state_country: String,
-lat_lon: String,
-phone: String,
-zip : String
-});
-
-var meetupChannel = new mongoose.Schema({
-  EventID 	: String,
-  Description : String,
-  Event_hosts :[{ type: mongoose.Schema.Types.ObjectId , ref: 'event_hosts' }],
-  Event_url : String,
-  GroupName : String,
-  GroupID 	: String,
-  Distance 	: String,
-  Venue		: [{ type: mongoose.Schema.Types.ObjectId , ref: 'venue' }],
-  Created 	: String,
-  Updated 	: String,
-  yes_rsvp_count : String
-});
-var MeetupChannel = mongoose.model('meetupChannel', meetupChannel);
-
-
-Lib.prototype.GetEvents = function() {
-
-	var _results=[];
-	meetup.getOpenEvents({ 'lon' : '-118.420615799005' ,  'lat' : '34.04430982618546', 'radius' : '200' }, function(err,events) {
-	for (var i = 0; i < events.results.length; i++ ) {
-				var ev = events.results[i];
-				var _muchannel = new MeetupChannel({
-				EventID:ev.id,
-				Description:ev.description,
-				Event_hosts:ev.event_hosts,
-				Event_url:ev.event_url,
-				Venue:ev.venue,
-				GroupName:ev.name,
-				GroupID:ev.group.id,
-				Distance:ev.distance,
-				Created:ev.created,
-				Updated:ev.updated,
-				yes_rsvp_count:ev.yes_rsvp_count
-			});
-			processEvents(_muchannel);
+var models = require('./valueObjects')(mongoose);
+///-------------------------------------------------------
+function Meetup() {}
+///-------------------------------------------------------
+function meetupError(error)
+{
+    console.dir('ErrorCode:'+error.code+' ErrorSetails:'+error.details+' ErrorProblem:'+error.problem );
+}
+Meetup.prototype.getEventsByLocation = function(location) {
+	meetup.getOpenEvents(location, function(err,events) {
+    if (err) meetupError(events);
+	if (typeof events.results != 'undefined'){
+		for (var i = 0; i < events.results.length; i++ ) {
+					var ev = events.results[i];
+					var _muChannel = new models.MeetupChannel({
+					EventID:ev.id,
+					Description:ev.description,
+					Event_hosts:ev.event_hosts,
+					Event_url:ev.event_url,
+					Venue:ev.venue,
+					GroupName:ev.name,
+					GroupID:ev.group.id,
+					Distance:ev.distance,
+					Created:ev.created,
+					Updated:ev.updated,
+					yes_rsvp_count:ev.yes_rsvp_count
+				});
+//				if (typeof ev.event_hosts != 'undefined') /* this will cause a throttle for a hour, must be done very carefully off line*/
+//				{
+//                    setTimeout(Meetup.prototype.GetUser({'member_id' : ev.event_hosts[0].member_id}), 3000);
+				//}
+				// Meetup.prototype.GetUser({'member_id' : _muChannel.Event_hosts.member_id});
+				Meetup.prototype.createMeetupChannel(_muChannel);
+			}
 		}
 	});
-}
-
-function processEvents(meetupChannel){
-	MeetupChannel.find({ EventID: meetupChannel.EventID }, function(err, _found) {
+}///-------------------------------------------------------
+Meetup.prototype.createMeetupChannel= function (meetupChannel){
+	models.MeetupChannel.find({ EventID: meetupChannel.EventID }, function(err, _found) {
 	  if (err) return console.error(err);
 	   if (_found.length > 0 ){ 
-		 console.log ('found event');
-			 MeetupChannel.remove({EventID: meetupChannel.EventID}, function(err, _found) {
+		  console.dir ('Found event '+ meetupChannel.EventID);
+			 models.MeetupChannel.remove({EventID: meetupChannel.EventID}, function(err, _found) {
 			 if (err) return console.error(err);
 				meetupChannel.save(function(err, meetupChannel) {
 				if (err) return console.error(err);
-				   //console.dir('Updated:[' + meetupChannel.EventID+'] '+meetupChannel.GroupName);
-				   console.dir(meetupChannel);
+				// console.dir('Updated:[' + meetupChannel.EventID+'] '+meetupChannel.GroupName+'Hosted by:' +meetupChannel.Event_hosts);
 				});				
 			 });
 		 }
-		if (_found.length == 0 ){
+		if (_found.length == 0 || typeof _found == 'undefined'){
 			meetupChannel.save(function(err, meetupChannel) {
 			if (err) return console.error(err);
-			 //console.dir('New Channel Saved:[' + meetupChannel.EventID+'] '+meetupChannel.GroupName);
-			 console.dir(meetupChannel);
+			console.dir ('created event '+ meetupChannel.EventID);
+			// console.dir('New Channel Saved:[' + meetupChannel.EventID+'] '+meetupChannel.GroupName+'Hosted by:' +meetupChannel.Event_hosts);
 			 });	
 		 }
 	}).limit(1);
 }
-module.exports = {"Lib" : Lib};
+///-------------------------------------------------------
+Meetup.prototype.createMeetupMember= function (meetupMember){
+	models.MeetupMember.find({ id: meetupMember.id }, function(err, _found) {
+	  if (err) return console.error(err);
+	   if (_found.length > 0 ){ 
+		 console.dir ('Found member');
+			 models.meetupMember.remove({id: meetupMember.EventID}, function(err, _found) {
+			 if (err) return console.error(err);
+				meetupMember.save(function(err, meetupMember) {
+				if (err) return console.error(err);
+				   console.dir('Updated:[' + meetupMember.id+'] aka: '+meetupMember.name );
+				});				
+			 });
+		 }
+		if (_found.length == 0 ){
+			meetupMember.save(function(err, meetupMember) {
+			if (err) return console.error(err);
+			 console.dir('New Member created:[' + meetupMember.EventID+'] '+meetupMember.name );
+			 });	
+		 }
+	}).limit(1);
+}
+///-------------------------------------------------------
+Meetup.prototype.GetUser=function(fields) {
+
+	meetup.getMembers(fields,function(err,member) {
+		if (err)
+            meetupError(member);
+			//console.dir(fields);
+			console.dir('member.name :: '+member.results[0].name);
+			if (typeof member != 'undefined'){
+				if (MeetupChannel){
+					new models.User.createMeetupUser(ConvertMemberToUser(member.results[0]));
+					// console.dir('member.results est');
+					var _member=member.results[0];
+					console.dir(_member.name+'|'+ _member.lon+','+_member.lat+'|');
+				}
+			}
+		});
+}
+function ConvertMemberToUser(meetupMember){
+	var _meetupMember = new models.MeetupMember({
+		  bio: meetupMember.bio,
+		  birthday :meetupMember.birthday,
+		  Country: meetupMember.Country, 
+		  City: meetupMember.City,
+		  State: meetupMember.State,
+		  visited : meetupMember.email,
+		  gender : meetupMember.gender,
+		  hometown : meetupMember.hometown,
+		  id : meetupMember.id,
+		  lang: meetupMember.lang,
+		  joined : meetupMember.joined,
+		  settings : meetupMember.settings,
+		  lat:meetupMember.lat, 
+		  lon:meetupMember.lon,
+		  name: meetupMember.name,
+		  photo:meetupMember.photo,
+		  visited :meetupMember.visited,
+		  loc: [meetupMember.lon,meetupMember.lat]
+	});
+
+		return new models.User({
+			name:meetupMember.name,
+			bio:meetupMember.bio,
+			email:meetupMember.email,
+			sourceType:'meetup',
+			sourceId:meetupMember.id,
+			loc: [meetupMember.lon,meetupMember.lat]
+		});
+
+}
+///-------------------------------------------------------
+module.exports = {"Meetup" : Meetup};
+///-------------------------------------------------------
--- Server/routes/valueObjects.js
+++ Server/routes/valueObjects.js
@@ -1,29 +1,9 @@
-var mongoose = require('mongoose');
-var testIlya='ilyaTest';
-
+module.exports = function(mongoose) {
 var messageSchema = mongoose.Schema({
 	fromUser : { type: mongoose.Schema.Types.ObjectId , ref: 'users' },
 	toUser : { type: mongoose.Schema.Types.ObjectId , ref: 'users' },
-	//channel : { type: mongoose.Schema.Types.ObjectId , ref: 'channels' },
 	message : String,
 	timestamp : { type: Date, default: Date.now }
-	
-});
-
-var userSchema = mongoose.Schema({
-  avatar : String,
-  cover  : String,
-  firstName: String,
-  lastName : String,
-  title : String,
-  bio 	: String,
-  email : String,
-  device : String,
-  pushId : String,
-  subscriptions : [{ type: mongoose.Schema.Types.ObjectId , ref: 'channels' }],
-  timestamp : { type: Date, default: Date.now },
-  settings : String,
-  loc: []
 });
 
 var channelPostSchema = mongoose.Schema({
@@ -38,8 +18,81 @@ var channelPostSchema = mongoose.Schema({
   fileUrl : String,
   timestamp : { type: Date, default: Date.now }
 });
+var event_hostsSchema = new mongoose.Schema({
+	member_id: String, 
+	member_name: String
+});
+var venueSchema = new mongoose.Schema({
+	address_1: String,
+	address_2: String,
+	address_3: String,
+	city_state_country: String,
+	lat_lon: String,
+	phone: String,
+	zip : String
+});
+var meetupChannelSchema = new mongoose.Schema({
+  EventID 	: String,
+  Description : String,
+  Event_hosts :[event_hostsSchema],
+  Event_url : String,
+  GroupName : String,
+  GroupID 	: String,
+  Distance 	: String,
+  Venue		: [venueSchema],
+  Created 	: String,
+  Updated 	: String,
+  yes_rsvp_count : String
+});
+var meetupMemberSchema = mongoose.Schema({
+  bio: String,
+  birthday : {day: String,month: String,year:String},
+  Country: String, 
+  City: String,
+  State: String,
+  email : String,
+  gender : String,
+  hometown : String,
+  id : String,
+  lang: String,
+  joined : { type: Date, default: Date.now },
+  settings : String,
+  lat:String, lon:String,
+  name: String,
+  photo:{highres_link:String,photo_id:String,photo_link:String,thumb_link:String},
+  visited : { type: Date, default: Date.now },
+  loc: []
+});
+meetupMemberSchema.index({ loc: '2d' });
 
-var channelPost = mongoose.model('channelPosts' , channelPostSchema);
+var userSchema = mongoose.Schema({
+  avatar : String,
+  cover  : String,
+  name: String,
+  title : String,
+  bio 	: String,
+  email : String,
+  device : String,
+  pushId : String,
+  sourceType:String,
+  sourceId:String,
+  subscriptions : [{ type: mongoose.Schema.Types.ObjectId , ref: 'channels' }],
+  timestamp : { type: Date, default: Date.now },
+  settings : String,
+  loc: []
+});
+userSchema.index({ loc: '2d' });
 
-var User = mongoose.model('users' , userSchema);
-var Message = mongoose.model('messages' , messageSchema);
+
+   var models = {
+	 ChannelPost : mongoose.model('channelPostSchema' , channelPostSchema),
+	 User : mongoose.model('userSchema' , userSchema),
+	 Message : mongoose.model('messageSchema' , messageSchema),
+	 Event_hostsSchema : mongoose.model('event_hostsSchema' , event_hostsSchema),
+	 VenueSchema: mongoose.model('venueSchema' , venueSchema),
+	 MeetupChannel : mongoose.model('meetupChannelSchema', meetupChannelSchema),
+	 MeetupMember : mongoose.model('meetupMemberSchema', meetupMemberSchema),
+    };
+    return models;
+}
+
